1187a014fafd0f7b40814cd23fffdac3acae39cb
Jorge <121674948+JorgeMonroy06@users.noreply.github.com>
2023-02-21 21:15:14 -0500

    Update main.dart

--- responsive_login_ui/lib/main.dart
+++ responsive_login_ui/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: SignUpView(),
     );
   }
