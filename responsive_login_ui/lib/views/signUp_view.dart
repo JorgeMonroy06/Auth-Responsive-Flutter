742002b9fd2a484d71ff142d48c2d1ddb776f150
Jorge <121674948+JorgeMonroy06@users.noreply.github.com>
2023-02-27 21:34:57 -0500

    Update signUp_view.dart

--- responsive_login_ui/lib/views/signUp_view.dart
+++ responsive_login_ui/lib/views/signUp_view.dart
@@ -233,6 +233,152 @@ class _SignUpViewState extends State<SignUpView> {
                 SizedBox(
                   height: size.height * 0.02,
                 ),
+                
+                /// Main Body
+  Widget _buildMainBody(
+      Size size, SimpleUIController simpleUIController, ThemeData theme) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment:
+          size.width > 600 ? MainAxisAlignment.center : MainAxisAlignment.start,
+      children: [
+        size.width > 600
+            ? Container()
+            : Lottie.asset(
+                'assets/wave.json',
+                height: size.height * 0.2,
+                width: size.width,
+                fit: BoxFit.fill,
+              ),
+        SizedBox(
+          height: size.height * 0.03,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Sign Up',
+            style: kLoginTitleStyle(size),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Create Account',
+            style: kLoginSubtitleStyle(size),
+          ),
+        ),
+        SizedBox(
+          height: size.height * 0.03,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                /// username
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Username',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+
+                  controller: nameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter username';
+                    } else if (value.length < 4) {
+                      return 'at least enter 4 characters';
+                    } else if (value.length > 13) {
+                      return 'maximum character is 13';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                /// Gmail
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email_rounded),
+                    hintText: 'gmail',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter gmail';
+                    } else if (!value.endsWith('@gmail.com')) {
+                      return 'please enter valid gmail';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
+
+                /// password
+                Obx(
+                  () => TextFormField(
+                    style: kTextFormFieldStyle(),
+                    controller: passwordController,
+                    obscureText: simpleUIController.isObscure.value,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock_open),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          simpleUIController.isObscure.value
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          simpleUIController.isObscureActive();
+                        },
+                      ),
+                      hintText: 'Password',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      } else if (value.length < 7) {
+                        return 'at least enter 6 characters';
+                      } else if (value.length > 13) {
+                        return 'maximum character is 13';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.01,
+                ),
+                Text(
+                  'Creating an account means you\'re okay with our Terms of Services and our Privacy Policy',
+                  style: kLoginTermsAndPrivacyStyle(size),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: size.height * 0.02,
+                ),
 
                 /// SignUp Button
                 signUpButton(theme),
