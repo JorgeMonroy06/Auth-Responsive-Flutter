917657d68630585dbfa3e424292fd3bc4cee7e2c
Jorge <121674948+JorgeMonroy06@users.noreply.github.com>
2023-03-08 20:23:31 -0500

    Update constants.dart

--- responsive_login_ui/lib/constants.dart
+++ responsive_login_ui/lib/constants.dart
@@ -5,13 +5,7 @@ TextStyle kLoginTitleStyle(Size size) => GoogleFonts.ubuntu(
       fontSize: size.height * 0.060,
       fontWeight: FontWeight.bold,
     );
-
-TextStyle kLoginSubtitleStyle(Size size) => GoogleFonts.ubuntu(
-      fontSize: size.height * 0.030,
-    );
-
-TextStyle kLoginTermsAndPrivacyStyle(Size size) =>
-    GoogleFonts.ubuntu(fontSize: 15, color: Colors.grey, height: 1.5);
+olors.grey, height: 1.5);
 
 TextStyle kHaveAnAccountStyle(Size size) =>
     GoogleFonts.ubuntu(fontSize: size.height * 0.022, color: Colors.black);
